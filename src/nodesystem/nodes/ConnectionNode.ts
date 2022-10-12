84f1c572b8bdca87475c8e732c24156a5603ac76
semvis123 <semvisscher10@gmail.com>
2022-10-12 18:54:17 +0200

    Add param to make splitter tiny

--- src/nodesystem/nodes/ConnectionNode.ts
+++ src/nodesystem/nodes/ConnectionNode.ts
@@ -8,7 +8,14 @@ import type { Metadata } from '../Metadata';
 import type { NodeParameter } from '../fullscreenPrompt/FullscreenPrompt';
 
 export class ConnectionNode extends Node {
-	parameters: NodeParameter[] = [];
+	parameters: NodeParameter[] = [
+		{
+			name: 'tiny',
+			label: 'Tiny',
+			checked: false,
+			type: 'checkbox'
+		}
+	];
 
 	constructor(id: string, x: number, y: number, public nodeSystem: NodeSystem, parameters?: NodeParameter[]) {
 		super(
@@ -22,6 +29,7 @@ export class ConnectionNode extends Node {
 			nodeSystem
 		);
 		this.importParams(parameters);
+		this.reset();
 	}
 
 	getMetadata(): Metadata {
@@ -39,18 +47,25 @@ export class ConnectionNode extends Node {
 		ctx.lineWidth = this.style.borderWidth;
 		ctx.lineJoin = 'round';
 		ctx.lineCap = 'round';
-		ctx.font = `${this.style.fontSize}px ${this.style.fontFamily}`;
-		ctx.textAlign = 'center';
-		ctx.textBaseline = 'middle';
 
 		const path = roundRect(0, 0, this.width, this.height, this.style.borderRadius);
 		ctx.stroke(path);
 		ctx.fill(path);
-
+		
 		this.renderConnectionPoints(ctx);
+		if (!this.getParamValue('tiny', false)) {
+			ctx.font = `${this.style.fontSize}px ${this.style.fontFamily}`;
+			ctx.textAlign = 'center';
+			ctx.textBaseline = 'middle';
+			ctx.fillStyle = this.style.fontColor;
+			ctx.fillText(`Splitter`, (this.width * 2) / 4, (this.height * 1) / 2);
+		}
+	}
 
-		ctx.fillStyle = this.style.fontColor;
-		ctx.fillText(`Splitter`, (this.width * 2) / 4, (this.height * 1) / 2);
+	reset() {
+		const tiny = this.getParamValue("tiny", false);
+		this.width = tiny ? 20 : 50;
+		this.height = tiny ? 15 : 40;
 	}
 
 	update() {
