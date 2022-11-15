98b91c3e2c754afceb24bf45e0eee8b7ad0ac7b8
semvis123 <semvisscher10@gmail.com>
2022-11-15 22:49:09 +0100

    Add logic to position floating modals

--- src/nodesystem/floatingModal/FloatingModal.ts
+++ src/nodesystem/floatingModal/FloatingModal.ts
@@ -1,6 +1,7 @@
 import type { NodeSystemEventHandler } from '../handlers/NodeSystemEventHandler';
 import { ToastMessage } from '../toastMessage/ToastMessage';
 import './floatingModal.css';
+import { FloatingModalPositioner } from './FloatingModalPositioner';
 
 export class FloatingModal {
 	htmlElement: HTMLDivElement;
@@ -8,12 +9,10 @@ export class FloatingModal {
 		x: 0,
 		y: 0
 	};
-	offsetMousePosition = {
-		x: 0,
-		y: 0
-	};
 	mouseIsDown = false;
 	mouseMoved: boolean;
+	x = 0;
+	y = 0;
 
 	constructor(public content: HTMLDivElement, eventHandler: NodeSystemEventHandler) {
 		this.htmlElement = document.createElement('div');
@@ -22,28 +21,30 @@ export class FloatingModal {
 		const closeButton = document.createElement('a');
 		closeButton.className = 'floating-modal-close-button';
 		closeButton.textContent = 'X';
-		closeButton.onmouseup = (e)=> {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-            eventHandler.addEventListeners();
-            this.remove();
-        }
+		closeButton.onmouseup = (e) => {
+			e.preventDefault();
+			e.stopImmediatePropagation();
+			eventHandler.addEventListeners();
+			this.remove();
+		};
 
 		this.htmlElement.onmousedown = (e) => {
 			this.mouseIsDown = true;
-			this.initialMousePosition.x = e.clientX - this.offsetMousePosition.x;
-			this.initialMousePosition.y = e.clientY - this.offsetMousePosition.y;
+			this.initialMousePosition.x = e.clientX - this.x;
+			this.initialMousePosition.y = e.clientY - this.y;
 			e.stopImmediatePropagation();
 			e.preventDefault();
 			eventHandler.cleanup();
 			window.addEventListener('mousemove', this.mousemove.bind(this));
 			this.mouseMoved = false;
+			FloatingModalPositioner.prototype.getInstance().bringToFront(this);
 		};
 		this.htmlElement.onmouseup = async (e) => {
+			if (!this.mouseIsDown) return;
 			this.htmlElement.style.cursor = 'grab';
 			this.mouseIsDown = false;
-			this.initialMousePosition.x = this.offsetMousePosition.x;
-			this.initialMousePosition.y = this.offsetMousePosition.y;
+			this.initialMousePosition.x = this.x;
+			this.initialMousePosition.y = this.y;
 			e.stopImmediatePropagation();
 			e.preventDefault();
 			window.removeEventListener('mousemove', this.mousemove.bind(this));
@@ -53,20 +54,21 @@ export class FloatingModal {
 				new ToastMessage('Copied to clipboard', 'info').show();
 			}
 		};
-
 		this.htmlElement.appendChild(closeButton);
 		this.htmlElement.appendChild(content);
 	}
 
 	show() {
 		document.body.appendChild(this.htmlElement);
+		FloatingModalPositioner.prototype.getInstance().addModal(this);
 	}
 
 	remove() {
 		this.htmlElement.remove();
+		FloatingModalPositioner.prototype.getInstance().removeModal(this);
 	}
 
-	private mousemove(e) {
+	private mousemove(e: MouseEvent) {
 		if (e.buttons == 0) return;
 		if (!this.mouseIsDown) return;
 		this.mouseMoved = true;
@@ -76,15 +78,29 @@ export class FloatingModal {
 		currentX = e.clientX - this.initialMousePosition.x;
 		currentY = e.clientY - this.initialMousePosition.y;
 
-		this.offsetMousePosition.x = currentX;
-		this.offsetMousePosition.y = currentY;
-
-		this.htmlElement.style.transform = 'translate3d(' + currentX + 'px, ' + currentY + 'px, 0)';
+		this.setPosition(currentX, currentY);
 		e.stopImmediatePropagation();
 		e.preventDefault();
 	}
 
 	copyableContent() {
 		return this.content.textContent;
+	}
+
+	setPosition(x: number, y: number) {
+		this.htmlElement.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0)';
+		this.x = x;
+		this.y = y;
+	}
+
+	overlaps(other: FloatingModal) {
+		const rect1 = this.htmlElement.getBoundingClientRect();
+		const rect2 = other.htmlElement.getBoundingClientRect();
+		return !(
+			rect1.right < rect2.left ||
+			rect1.left > rect2.right ||
+			rect1.bottom < rect2.top ||
+			rect1.top > rect2.bottom
+		);
 	}
 }
---
+++ src/nodesystem/floatingModal/FloatingModalPositioner.ts
@@ -0,0 +1,40 @@
+import type { FloatingModal } from './FloatingModal';
+
+export class FloatingModalPositioner {
+	sharedInstance: FloatingModalPositioner;
+	modals: FloatingModal[];
+
+	private constructor() {
+		this.modals = [];
+	}
+
+	getInstance(): FloatingModalPositioner {
+		if (!this.sharedInstance) this.sharedInstance = new FloatingModalPositioner();
+		return this.sharedInstance;
+	}
+
+	addModal(modal: FloatingModal) {
+		this.modals.push(modal);
+		this.positionModal(modal);
+	}
+
+	positionModal(modal: FloatingModal) {
+		// move overlapping modals to the left.
+		let overlappingModal = this.modals.find((m) => m !== modal && m.overlaps(modal));
+		while (overlappingModal) {
+			const x = overlappingModal.x - overlappingModal.htmlElement.getBoundingClientRect().width - 16;
+			modal.setPosition(x, modal.y);
+			overlappingModal = this.modals.find((m) => m !== modal && m.overlaps(modal));
+		}
+	}
+
+	removeModal(modal: FloatingModal) {
+		this.modals = this.modals.filter((m) => m !== modal);
+	}
+
+	bringToFront(modal: FloatingModal) {
+		this.modals = this.modals.filter((m) => m !== modal);
+		this.modals.push(modal);
+		this.modals.forEach((m, i) => (m.htmlElement.style.zIndex = 1000 + i.toString()));
+	}
+}
