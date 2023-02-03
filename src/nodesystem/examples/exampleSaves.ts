8e2c3a0cdc116c0faf0a4386043452a0f906cd76
semvis123 <semvisscher10@gmail.com>
2023-02-04 00:04:13 +0100

    Add calculator to examples

--- src/nodesystem/examples/exampleSaves.ts
+++ src/nodesystem/examples/exampleSaves.ts
@@ -2,11 +2,13 @@ import * as introduction from './introduction.json';
 import * as segmentDisplay from './7-segment-display.json';
 import * as RisingEdgeDetector from './rising-edge-detector.json';
 import * as FallingEdgeDetector from './falling-edge-detector.json';
+import * as calculator from './calculator.json';
 import type { NodeSaveFile } from '../NodeSaveFile';
 
 export const exampleSaves: { filename: string; save: NodeSaveFile }[] = [
 	{ filename: 'Introduction', save: introduction as NodeSaveFile },
 	{ filename: '7 Segment-display', save: segmentDisplay as NodeSaveFile },
 	{ filename: 'Rising edge detector', save: RisingEdgeDetector as NodeSaveFile },
-	{ filename: 'Falling edge detector', save: FallingEdgeDetector as NodeSaveFile }
+	{ filename: 'Falling edge detector', save: FallingEdgeDetector as NodeSaveFile },
+	{ filename: 'Calculator', save: calculator as NodeSaveFile },
 ];
